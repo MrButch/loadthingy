587cab132431fa8893e57fbdb5ac78fc526f6e87
storkmanthecool <33793468+MrButch@users.noreply.github.com>
2020-06-16 23:59:05 -0500

    fea

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -10,7 +10,7 @@
 // Leave it blank to use text instead
 // (Put the files in the images folder)
 // (You can also use external URL)
-var l_serverImage = "https://www.dropbox.com/s/irxhq86zkh6o18o/gay.png?dl=1";
+var l_serverImage = "nfea.png";
 
 // Center logo?
 // 'true' or 'false'
